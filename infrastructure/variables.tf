df666a0a148d838bfcc0cef58bb6c87ff549a389
Binayak104 <binayaksahu222@gmail.com>
2026-02-07 19:57:24 +0530

    Cleanup: Remove legacy monolithic Terraform files

--- infrastructure/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  description = "AWS Region"
-  default     = "us-east-1"
-}
-
-variable "environment" {
-  description = "Deployment environment (dev, qa, prod)"
-  default     = "dev"
-}
